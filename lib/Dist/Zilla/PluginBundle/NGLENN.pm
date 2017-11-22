464ec8c309f7b5e37b22c47cce90082e7d0e08c7
Nathan Glenn <nathan-gary.glenn@trivago.com>
2017-11-22 17:40:09 +0100

    After release: bump $VERSION and timestamp Changes

--- lib/Dist/Zilla/PluginBundle/NGLENN.pm
+++ lib/Dist/Zilla/PluginBundle/NGLENN.pm
@@ -4,7 +4,7 @@ use warnings;
 package Dist::Zilla::PluginBundle::NGLENN;
 # ABSTRACT: Dist::Zilla plugin configuration for me
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 # Dependencies
 use Moose 0.99;
